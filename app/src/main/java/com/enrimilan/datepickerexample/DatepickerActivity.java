9d5a5632c3901df584b0c1f3aa92fdf0488cc871
Enri Miho <enrimiho@gmail.com>
2016-03-05 01:59:49 +0100

    handle the button click by showing a datePickerFragment and update the title of the action bar depending on the chosen date.

--- app/src/main/java/com/enrimilan/datepickerexample/DatepickerActivity.java
+++ app/src/main/java/com/enrimilan/datepickerexample/DatepickerActivity.java
@@ -1,14 +1,18 @@
 package com.enrimilan.datepickerexample;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Calendar;
+
 public class DatepickerActivity extends AppCompatActivity {
 
     private Button button;
+    private DialogFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,32 @@ public class DatepickerActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO handle the click here
+                showDatePickerDialog();
             }
         });
+    }
+
+    /**
+     * Handle the new set date here.
+     * @param year
+     * @param month
+     * @param day
+     */
+    public void onDateSet(int year, int month, int day){
+        Calendar c = Calendar.getInstance();
+        if(year == c.get(Calendar.YEAR) && month == c.get(Calendar.MONTH) &&
+                day == c.get(Calendar.DAY_OF_MONTH)) {
+            button.setText(getString(R.string.today));
+        }
+        else {
+            button.setText(day+"."+(month+1)+"."+year);
+        }
+    }
+
+    private void showDatePickerDialog(){
+        if(fragment == null) {
+            fragment = new DatePickerFragment();
+        }
+        fragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
